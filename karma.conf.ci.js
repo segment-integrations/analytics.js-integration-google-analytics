4a7c66b62a924c225e75f38efad34b491fda7f15
Peter Richmond <peterrichmond@Peters-MacBook-Pro.local>
2017-03-09 13:11:11 -0800

    Temporarily raising disconnect tolerance for Karma.

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -72,7 +72,8 @@ module.exports = function(config) {
   }
 
   config.set({
-    browserDisconnectTolerance: 1,
+    //FIXME(peripheral1994): Builds as of 2017-03-07 are having problems with disconnects. Attempting to rectify.
+    browserDisconnectTolerance: 3,
 
     singleRun: true,
 
